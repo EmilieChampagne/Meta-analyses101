0b90e26d0472395d3cfea9d8aac3486fcccab094
EmilieChampagne <e.champagne.1@gmail.com>
2019-04-18 16:20:53 -0400

    Minor updates

--- Exemples.R
+++ Exemples.R
@@ -6,7 +6,8 @@ library(metafor)
 #of neighboring plants influences the strength of associational effects 
 #on mammal herbivory. Ecosphere 7:e01371.
 
-Data <-read.table(file="Exemple.txt", header= TRUE, na.string= ".", stringsAsFactors = FALSE)
+Data <-read.table(file="Data.txt", header= TRUE, 
+                  colClasses = c("factor", "factor", rep("numeric", 7)))
 Data
 #Year: Année de publication de l'article
 #Type_exp: Type d'étude (Observation ou Expérimental)
@@ -14,3 +15,5 @@ Data
 #Mean_C, N_C, SD_C: Moyenne, n et SD du contrôle (Dommage par des herbivore de la plante focale sans plante compagne)
 #Mean_E, N_E, SD_E: Moyenne, n et SD du traitement (Dommage par des herbivore de la plante focale avec plante compagne)
 
+escalc(measure="SMD", m1i= Mean_C, m2i= Mean_E, sd1i= SD_C, sd2i= SD_E,
+       n1i= N_C, n2i= N_E, data= Data)
